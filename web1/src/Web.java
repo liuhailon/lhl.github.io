faa54878f3f0b56f5f5e0468e3441b277b82d64f
liuhailon <2823267150@qq.com>
2018-08-09 10:20:50 +0800

    这是web项目

---
+++ web1/src/Web.java
@@ -0,0 +1,18 @@
+import java.util.concurrent.Callable;
+
+public class Web {
+    public static void main(String[] args) {
+        System.out.print("");
+        //Ctrl+/表示单行注释
+        //Ctrl+Shift+/表示多行注释
+        //Ctrl+d表示复制行
+        //Ctrl+x表示删除行
+        //Ctrl+Alt+s打开设置
+        //Ctrl+w选中代码
+        //Ctrl+f4关闭此资源
+        //Ctrl+空格代码提示
+        //Ctrl+Shift+空格补全代码
+        //
+    }
+}
+
---
+++ web1/web/index.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: lhl
+  Date: 2018/8/8
+  Time: 19:03
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>$Title$</title>
+  </head>
+  <body>
+  <a href="http://www.baidu.com">百度一下</a>
+  </body>
+</html>
